d5182387bb5132c5b582f48d3922edb405e66b38
ozeascelta <162350746+ozeascelta@users.noreply.github.com>
2024-03-09 20:33:25 -0300

    Saas codigo

---
+++ frontend/build/precache-manifest.a29745486fdfc2ef6421b6e2f4b548b9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2deb69890972f331d01c83dde5c49389",
+    "url": "/index.html"
+  },
+  {
+    "revision": "786e4e534143ea69237a",
+    "url": "/static/css/2.8497de37.chunk.css"
+  },
+  {
+    "revision": "19d794bcbdc9c88f7134",
+    "url": "/static/css/main.1a98c101.chunk.css"
+  },
+  {
+    "revision": "786e4e534143ea69237a",
+    "url": "/static/js/2.5f3dcfad.chunk.js"
+  },
+  {
+    "revision": "7bd28c3f53bd26549396caa128a4efbe",
+    "url": "/static/js/2.5f3dcfad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d02ee7d312e4a018a23",
+    "url": "/static/js/3.7a52e510.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/static/js/3.7a52e510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d794bcbdc9c88f7134",
+    "url": "/static/js/main.cb813901.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.cb813901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0472789977328e37aac1",
+    "url": "/static/js/runtime-main.a77f7866.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "f0d6a963359a856b71f24dcbee06fab6",
+    "url": "/static/media/logo.f0d6a963.png"
+  },
+  {
+    "revision": "5c788b8d54d3ebf021114528116dfd77",
+    "url": "/static/media/n8n.5c788b8d.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "8244f0423cb09d847ac99e7546cb9220",
+    "url": "/static/media/wa-background-dark.8244f042.png"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  },
+  {
+    "revision": "e5374d73ff43538fdaa16a3f1a980f4b",
+    "url": "/static/media/webhook.e5374d73.png"
+  }
+]);
